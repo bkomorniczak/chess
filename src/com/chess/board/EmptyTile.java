d5f22395aac17c73fd8c8fa41528a51689027bb5
bkomorniczak <b.komorniczak@gmail.com>
2020-03-29 20:00:10 +0200

    Tile updated

--- src/com/chess/board/EmptyTile.java
+++ src/com/chess/board/EmptyTile.java
@@ -11,9 +11,7 @@ public  final class EmptyTile extends Tile{
     private EmptyTile(int coordinates, boolean isOccupied) {
         super(coordinates, false);
     }
-    public static Tile createTile(final int coordinates, final Piece piece){
-        return piece != null ? new OccupiedTile(coordinates, true, piece) : EMPTY_TILE_MAP.get(coordinates);
-    }
+
 
     @Override
     public Piece getPiece() {
@@ -24,13 +22,4 @@ public  final class EmptyTile extends Tile{
         return false;
     }
 
-    private static final Map<Integer,EmptyTile> EMPTY_TILE_MAP = createAllEmptyTiles();
-
-    private static Map<Integer, EmptyTile> createAllEmptyTiles(){
-        final Map<Integer,EmptyTile> emptyTileMap = new HashMap <>();
-        for(int i =0; i<64;i++){
-            emptyTileMap.put(i, new EmptyTile(i,false));
-        }
-        return emptyTileMap;
-    }
 }
--- src/com/chess/board/Tile.java
+++ src/com/chess/board/Tile.java
@@ -2,6 +2,9 @@ package com.chess.board;
 
 import pieces.Piece;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public abstract class Tile {
 
     protected final int coordinates;
@@ -20,5 +23,17 @@ public abstract class Tile {
 
     public abstract Piece getPiece();
 
+    public static Tile createTile(final int coordinates, final Piece piece){
+        return piece != null ? new OccupiedTile(coordinates, true, piece) : EMPTY_TILE_MAP.get(coordinates);
+    }
+    private static final Map<Integer,EmptyTile> EMPTY_TILE_MAP = createAllEmptyTiles();
+
+    private static Map<Integer, EmptyTile> createAllEmptyTiles(){
+        final Map<Integer,EmptyTile> emptyTileMap = new HashMap<>();
+        for(int i =0; i<64;i++){
+            emptyTileMap.put(i, new EmptyTile(i,false));
+        }
+        return emptyTileMap;
+    }
 
 }
